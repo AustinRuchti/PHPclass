f9f67dd830f02c30618e9d513ddc04cacb0936e7
Austin Ruchti <ruchta67@uwosh.edu>
2018-02-07 17:54:47 -0600

    Start of PHP class

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Class - Austin Ruchti</title>
+    <link rel="stylesheet" type="text/css" href="css/mystyles.css"/>
+</head>
+
+<body>
+<div class="page-wrap">
+    <header class="site-header">
+        <h1>PHP Class - Austin Ruchti</h1>
+    </header>
+    <div class="flex-box">
+    <nav class="main-nav">
+        <ul>
+            <li class="menu-item"><a href="/">Home</a></li>
+            <li class="menu-item"><a href="#">Loops</a></li>
+            <li class="menu-item"><a href="#">Strings</a></li>
+        </ul>
+    </nav>
+    <section class="main-content">
+        <div class="img">/*insert image and tag*/</div>
+        <h2>Hello! (insert content here)</h2>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Proin tempus pellentesque est maximus sodales. Cras pellentesque augue porta
+                accumsan sollicitudin.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Proin tempus pellentesque est maximus sodales. Cras pellentesque augue porta
+                accumsan sollicitudin.</p>
+        </div>
+    </section>
+    <aside class="right-sidebar">Sidebar</aside>
+    </div><!--end flex box here-->
+
+    <footer>
+        <div>Learn PHP<sup>&copy;</sup> 2018</div>
+    </footer>
+</div><!--/page wrap-->
+</body>
+</html>
+
+
